2c25b40aed09311de0e39ca62206fad86099874a
prakash-goatrobotics <prakash@goat-robotics.com>
2025-06-22 10:03:00 +0530

    add math concepts

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/prakash-goatrobotics/mathMod
+
+go 1.24.2
---
+++ mathModPublic.go
@@ -0,0 +1,19 @@
+package mathModPublic
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
+
+func Mul(a, b int) int {
+	return a * b
+}
+
+func Div(a, b int) int {
+	return a / b
+}
+
+
